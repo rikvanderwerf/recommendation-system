f74a8a00dc9f37be83fcc68c5b63538f65442efc
Rik van der Werf <rikvanderwerf@outlook.com>
2016-02-09 10:58:17 +0100

    import data file

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created by rikvanderwerf on 9-2-16.
  */
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            openFile("./data/userItem.data");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
-    private void importData() {
-        
+    public static void openFile(String path) throws IOException {
+        FileReader fr = new FileReader(path);
+        BufferedReader textReader = new BufferedReader(fr);
+
+        String line;
+        while ((line = textReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        textReader.close();
     }
 }
